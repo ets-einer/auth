37fe54108ff7cfaa8a4613959c46abd512beddaf
abehidek <hidek.abe@outlook.com>
2022-12-22 14:42:50 -0300

    feat(web/signin): add callback url when session is created

--- web/src/pages/signin.tsx
+++ web/src/pages/signin.tsx
@@ -1,11 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 function SignIn() {
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
   const navigate = useNavigate();
+  let [searchParams, setSearchParams] = useSearchParams();
   const { mutate } = useMutation(
     ["signIn"],
     () =>
@@ -19,7 +20,13 @@ function SignIn() {
         credentials: "include",
       }).then((res) => res.json()),
     {
-      onSuccess: (res) => alert(JSON.stringify(res, null, 2)),
+      onSuccess: (res) => {
+        // alert(JSON.stringify(res, null, 2));
+        const callBackUrl = searchParams.get("callbackUrl");
+        if (callBackUrl) {
+          window.location.href = callBackUrl;
+        }
+      },
       onError: (err) => alert(JSON.stringify(err, null, 2)),
     }
   );
